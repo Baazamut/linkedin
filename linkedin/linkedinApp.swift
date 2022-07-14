d6a2f0bb72767b318ba842149279744817e0686a
Boury <bouryj@hotmail.fr>
2022-07-14 15:00:04 +0200

    Initial Commit

---
+++ linkedin/linkedinApp.swift
@@ -0,0 +1,17 @@
+//
+//  linkedinApp.swift
+//  linkedin
+//
+//  Created by Julien  Boury on 14/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct linkedinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
